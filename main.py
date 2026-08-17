ae8ec1f8075abc494acd97c400a32160677f0d40
Dejiaali <maariyaali212@gmail.com>
2026-08-17 10:25:44 +0300

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import streamlit as st
 
-st.title(" Adnan's Travels")
-st.write(" Intercity buses, daily departures")
-st.info(" Booking office :6am - 10pm")
-Passenger = st.text_input(" Passenger ")
-if Passenger:
-    st.write(f" THe Passenger is: {Passenger}")
+
+with st.form(" Registration"):
+    st.title(" Adnan's Travels")
+    st.write("Intercity buses, daily departure")
+    st.success(" Booking office 6am - 10pm")
+    passenger = st.text_input("passenger")
+    destination = st.text_input(" destination")
+    seat = st.radio("seat", ["Standard", "Premium"])
+   
+    extra = st.checkbox("Extra luggage")
+    
+
+    submitted = st.form_submit_button("Book")
+    if submitted:
+        st.success("Booking Confirmed!")
+        st.write(f" {passenger} , {destination}")
+        st.write(f"Seat:{seat} ")
+        if extra:
+            st.write(" Extra luggage: Yes")
+        else:
+            st.write(" Extra luggage: No")
+    
+    
